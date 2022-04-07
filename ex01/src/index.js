df48b301c9917bfac713b94183d0fa07d2877caa
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-07 14:00:53 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+// Create an array here
+var myArr = [2, 4, 0, 8, 10];
+// End of creating an array
+// Create a function below this line
+function myArrayFunction(myItems) {
+  myItems = myArr;
+  myArr[2] = 6;
+
+  return myItems;
+}
+console.log(myArrayFunction(myArr));
+module.exports = myArrayFunction;
