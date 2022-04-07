e018ce9926d7acb98aa25b0a17ad438a08415d39
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-07 19:33:35 +0200

    Vjezba 12

---
+++ ex12/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+function splitArrayInGroups(arr, n) {
+  var result = [];
+
+  for (var i = 0; i < n; i++) {
+    result.push(arr.slice(i * n, (i + 1) * n));
+  }
+
+  return result;
+}
+
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+
+// Only change code above this line
+module.exports = splitArrayInGroups;
